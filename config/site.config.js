be8fbefe9fa23888e6fb76cea7a26fd85b67da5e
Helio-ZhouMo <141138570+Helio-ZhouMo@users.noreply.github.com>
2024-03-05 21:57:19 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -56,8 +56,8 @@ module.exports = {
       link: 'https://github.com/spencerwooo/onedrive-vercel-index',
     },{
       name: 'bilbil',
-      link: 'https://space.bilibili.com/359856304'，
-    }，
+      link: 'https://space.bilibili.com/359856304',
+    },
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
